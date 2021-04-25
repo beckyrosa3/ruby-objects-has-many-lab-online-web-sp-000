082934eece84d5ddf0aafbb9bda05edd3e424150
Becky Rosa <becky_m_rosa@yahoo.com>
2021-04-25 19:56:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,17 @@ class Song
 
   attr_accessor :title, :artist
 
+  @@all = []
+
   def initialize(title)
     @title = title
   end
 
+  def save
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
 end
