ac84a6f3b5fef4238f71670743284497b52221c8
Becky Rosa <becky_m_rosa@yahoo.com>
2021-04-25 20:03:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,7 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @name << @songs
+  
   end
 
   def add_song(song)
@@ -13,8 +13,8 @@ class Artist
     song.artist = self
   end
 
-  def songs
-    @songs
-  end
+  #def songs
+  #  @songs
+  #end
 
 end
